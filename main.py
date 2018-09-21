7e2bd2f5924fc11e2178cdd4d270a8f61c3f08d3
runderwood <runderwood2016@gmail.com>
2018-09-21 14:59:54 -0500

    working on gathering form information

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 from flask import Flask, request, redirect, render_template
 import cgi
 
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+@app.route("/", methods = ['POST'])
+def check():
+    uName = request.userInfo["uName"]
+    eMail = request.userInfo["eMail"]
+    password = request.userInfo["password"]
+    confirmPassword = request.userInfo["confirmPassword"]
+    uNameError = ""
+    eMailError = ""
+    passwordError = ""
+    confirmPasswordError = ""
+
+    if (uName == "")
+        uNameError = "You must insert a username"
+    if (password == "")
+        passwordError = "You must insert a password"
+    if (confirmPassword == "")
+        confirmPasswordError = "You must insert a confirm password"
+
+    
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+app.run()
