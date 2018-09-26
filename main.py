fb1554cbdf2918cbe1f3f6df1a023ffcc291e046
runderwood <runderwood2016@gmail.com>
2018-09-26 15:02:13 -0500

    added an error check & created base.html

--- main.py
+++ main.py
@@ -18,97 +18,101 @@ def check():
     numErrors = 0
 
     #Check to confirm username
-    if (uName == "")
+    if uName == "":
         uNameError = "You must insert a username"
         ++numErrors
-    else
-        if (" " in uName)
+    else:
+        if " " in uName:
             uNameError = "There can be no space"
             ++numErrors
-        if (len(uName) < 3 || len(uName) > 20)
-            if (uNameError == "")
+        if len(uName) < 3 or len(uName) > 20:
+            if uNameError == "":
                 uNameError = "Username must be 3-20 characters long"
                 ++numErrors
-            else
+            else:
                 uNameError += ", Username must be 3-20 characters long"
                 ++numErrors
 
     #Check to confirm password
-    if (password == "")
+    if password == "":
         passwordError = "You must insert a password"
         ++numErrors
-    else 
-        if (" " in password)
+    else:
+        if " " in password:
             passwordError = "There can be no space"
             ++numErrors
-        if (len(password) < 3 || len(password) > 20)
-            if (passwordError == "")
+        if len(password) < 3 or len(password) > 20:
+            if passwordError == "":
                 passwordError = "Passwod must be 3-20 characters long"
                 ++numErrors
-            else
+            else:
                 passwordError += ", Password must be 3-20 characters long"
                 ++numErrors
 
     #Check to verify confirmpassword
-    if (confirmPassword == "")
+    if confirmPassword == "":
         confirmPasswordError = "You must insert a confirm password"
         ++numErrors
-    else 
-        if (confirmPassword == password)
-            if (" " in confirmPassword)
+    else:
+        if confirmPassword == password:
+            if " " in confirmPassword:
                 confirmPasswordError = "There can be no space"
                 ++numErrors
-            if (len(confirmPassword) < 3 || len(confirmPassword) > 20)
-                if (confirmPasswordError == "")
+            if len(confirmPassword) < 3 or len(confirmPassword) > 20:
+                if confirmPasswordError == "":
                     confirmPasswordError = "Passwod must be 3-20 characters long"
                     ++numErrors
-                else
+                else:
                     confirmPasswordError += ", Password must be 3-20 characters long"
                     ++numErrors
-        else
+        else:
             confirmPasswordError = "Your confirm password and password do not match"
             ++numErrors
 
     #Check to verify e-mail
-    if (eMail != "")
-        if (" " in eMail)
+    if eMail != "":
+        if " " in eMail:
             eMailError = "Your e-mail can not contain a space"
             ++numErrors
-        if (len(eMail) < 3 || len(eMail) > 20)
-            if (eMailError == "")
+        if len(eMail) < 3 or len(eMail) > 20:
+            if eMailError == "":
                 eMailError = "Your e-mail must be 3-20 characters long"
                 ++numErrors
-            else
+            else:
                 eMailError += ", Your e-mail must be 3-20 characters long"
                 ++numErrors
 
-        for letter in eMail
+        for letter in eMail:
             atSignFound = False
             dotFound = False
 
-            if (letter == "@")
-                if (atSignFound == False)
+            if letter == "@":
+                if atSignFound == False:
                     atSignFound = True
-                else
-                    if (eMailError == "")
+                else:
+                    if eMailError == "":
                         eMailError = "You can only have one @ in your e-mail"
                         ++numErrors
-                    else 
+                    else: 
                         eMailError += ", You can only have one @ in your e-mail"
                         ++numErrors
-            if (letter == ".")
-                if (dotFound == False)       
+            if letter == ".":
+                if dotFound == False:       
                     dotFound = True
-                else 
-                    if (eMailError == "")
+                else: 
+                    if eMailError == "":
                         eMailError = "You can only have one . in your e-mail"
                         ++numErrors
-                    else 
-                        eMailError += ", You can only have on . in your e-mail"
-                        ++numErrors      
-                
+                    else:
+                        eMailError += ", You can only have one . in your e-mail"
+                        ++numErrors    
 
-    
+    #checking to see if any errors were caught and then deciding which template to return
+    if numErrors == 0:
+        return render_template("welcome.html", uName = uName)
+    else:
+        return render_template("index.html", uName = uName, eMail = eMail, uNameError = uNameError, eMailError = eMailError, passwordError = passwordError, confirmPasswordError = confirmPasswordError)
+                
 @app.route("/")
 def index():
     return render_template('index.html')
